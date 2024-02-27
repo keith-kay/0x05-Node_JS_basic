8a44dc990f7ee3abc56841f68bcaa1ef8d110376
keith-kay <keithrhova@gmail.com>
2024-02-28 00:11:54 +0300

    task 02

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,16 +1,13 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-if (process.stdin.isTTY) {
-  process.stdin.on('data', (data) => {
-    process.stdout.write(`Your name is: ${data.toString()}`);
-    process.exit();
-  });
-} else {
-  process.stdin.on('data', (data) => {
-    process.stdout.write(`Your name is: ${data.toString()}`);
-    process.exit();
-  });
-  process.on('exit', () => {
-    process.stdout.write('This important software is now closing\n');
-  });
-}
+process.stdin.on('readable', () => {
+  const uname = process.stdin.read();
+
+  if (uname) {
+    process.stdout.write(`Your name is: ${uname}`);
+  }
+});
+
+process.stdin.on('close', () => {
+  process.stdout.write('This important software is now closing\n');
+});
